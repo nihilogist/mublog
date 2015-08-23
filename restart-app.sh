b79856741fc3786ec63f258d04a84faaf7beab90
David Heaton <nihilogist@github.com>
2015-08-23 11:19:15 +0100

    Cleanup naming

--- restart-app.sh
+++ restart-app.sh
@@ -2,10 +2,6 @@
 #Stop any running instances
 docker-compose stop
 
-#remove zeromq master
-docker rm mublog_zeromqmain_1
-docker rmi mublog_zeromqmain
-
 #remove docker container for main server
 docker rm mublog_mongrel2_1
 docker rmi mublog_mongrel2
